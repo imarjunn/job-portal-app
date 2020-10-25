4b19b0063c4a323a572135794b0993df3ca34547
imarjunn <singh.arjun535@gmail.com>
2020-10-25 23:18:04 +0530

    Updates

---
+++ precache-manifest.ebd8dbd00b4ab8532750c979e8e813e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2adb912dd3daca2301236479eb319e18",
+    "url": "/job-portal-app/index.html"
+  },
+  {
+    "revision": "c13b2043265d7b13ab77",
+    "url": "/job-portal-app/static/css/main.c2a3b39d.chunk.css"
+  },
+  {
+    "revision": "0c3975fbb005aff18022",
+    "url": "/job-portal-app/static/js/2.2e2909b0.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/job-portal-app/static/js/2.2e2909b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13b2043265d7b13ab77",
+    "url": "/job-portal-app/static/js/main.1595c346.chunk.js"
+  },
+  {
+    "revision": "25853a826a75914ce4e3",
+    "url": "/job-portal-app/static/js/runtime-main.e9ab957c.js"
+  }
+]);
